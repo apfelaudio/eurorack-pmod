e83ec3f5ef69940ab30ed50affa4a7a4e3569946
Sebastian Holzapfel <me@vk2seb.com>
2022-07-10 15:13:42 +0200

    Verified sdin1, sdout1 and analog shifters work (first 2 channels only)

--- gateware/ak4619-cfg/ak4619-cfg.c
+++ gateware/ak4619-cfg/ak4619-cfg.c
@@ -8,7 +8,7 @@ const uint8_t reg_default_values[I2C_N_BYTES] = {
     0x20, // I2C slave address and RW bit (W=0).
     0x00, // Register address to start at.
 
-    0x00, // 0x00 Power Management
+    0x17, // 0x00 Power Management
     0x0C, // 0x01 Audio I/F Format
     0x0C, // 0x02 Audio I/F Format
     0x00, // 0x03 System Clock Setting
@@ -19,9 +19,9 @@ const uint8_t reg_default_values[I2C_N_BYTES] = {
     0x30, // 0x08 ADC2 Lch Digital Volume
     0x30, // 0x09 ADC2 Rch Digital Volume
     0x00, // 0x0A ADC Digital Filter Setting
-    0x00, // 0x0B ADC Analog Input Setting
+    0x55, // 0x0B ADC Analog Input Setting
     0x00, // 0x0C Reserved
-    0x00, // 0x0D ADC Mute & HPF Control
+    0x06, // 0x0D ADC Mute & HPF Control
     0x18, // 0x0E DAC1 Lch Digital Volume
     0x18, // 0x0F DAC1 Rch Digital Volume
     0x18, // 0x10 DAC2 Lch Digital Volume
--- gateware/top.v
+++ gateware/top.v
@@ -1,20 +1,49 @@
 module top (
-    input CLK,   // 12mhz
-    output P2_1, // SCL
-    inout P2_2,  // SDA
-    output P2_3  // PDN
+    input CLK,    // 12mhz
+    output P2_1,  // SCL
+    inout P2_2,   // SDA
+    output P2_3,  // PDN
+    output P2_4,  // MCLK
+    output P2_7,   // SDIN1
+    input P2_8,  // SDOUT1
+    output P2_9,  // LRCK
+    output P2_10  // BICK
 );
 
 reg [7:0] clkdiv = 8'd1;
 reg clk_i2c_x2 = 1'b0;
-reg pdn_out = 1'b0;
+reg pdn_out = 1'b1;
+
+reg lrck = 1'b0;
+reg bick = 1'b0;
 
 always @(posedge CLK) begin
     clkdiv <= clkdiv + 1;
-    if (clkdiv == 0) begin
-        clk_i2c_x2 <= ~clk_i2c_x2;
-        pdn_out <= 1'b1;
+    clk_i2c_x2 <= clkdiv[7];
+    bick <= clkdiv[1];
+    lrck <= clkdiv[7];
+end
+
+
+reg [15:0] out_val = 16'd0;
+reg [3:0] shift = 4'h0;
+always @(posedge lrck) begin
+    out_val <= out_val + 4'h8;
+end
+
+reg sdin1 = 1'b0;
+reg prev_lrck = 1'b0;
+always @(negedge bick) begin
+    if (prev_lrck != lrck) begin
+        shift <= 0;
     end
+    if (shift != 4'hF) begin
+        shift <= shift + 1;
+        sdin1 <= 1'b1 & (out_val >> (4'hF - shift));
+    end else begin
+        sdin1 <= 0;
+    end
+    prev_lrck <= lrck;
 end
 
 wire scl;
@@ -23,6 +52,12 @@ wire sda_in = 1'b1;
 assign P2_1 = scl ? 1'bz : 1'b0;
 assign P2_2 = sda_out ? 1'bz : 1'b0;
 assign P2_3 = pdn_out;
+
+assign P2_4 = CLK;
+assign P2_9 = lrck;
+assign P2_10 = bick;
+
+assign P2_7 = sdin1;
 
 
 i2cinit i2cinit_instance (
