f155843119c54f3e126a78030e9a79f0821df0ce
Sebastian Holzapfel <me@vk2seb.com>
2022-07-10 11:55:40 +0200

    Move all logic into 1 always block

--- gateware/i2cinit.v
+++ gateware/i2cinit.v
@@ -25,15 +25,7 @@ reg [2:0] i2cinit_state = I2CINIT_WAIT;
 
 reg clk_cnt = 1'b0;
 reg clk_scl = 1'b0;
-reg clk_sda = 1'b1;
-always @(posedge clk) begin
-    clk_cnt <= clk_cnt + 1;
-    if (clk_cnt == 0) begin
-        clk_scl <= ~clk_scl;
-    end else begin
-        clk_sda <= ~clk_sda;
-    end
-end
+reg clk_sda = 1'b0;
 
 reg scl_en = 1'b0;
 reg sda_wr = 1'b1;
@@ -44,52 +36,58 @@ assign sda_out = ~(~sda_wr || ~sda_startstop);
 reg [15:0] wait_cycles = 16'd0;
 reg [7:0] cur_byte  = 8'd0;
 reg [2:0] cur_shift = 3'd0;
-always @(posedge clk_sda) begin
-    if (i2cinit_state == I2CINIT_WAIT) begin
-        // TODO: when to start scl_en?
-        wait_cycles = wait_cycles + 1;
-        if (wait_cycles == 16'd20) begin
-            i2cinit_state <= I2CINIT_START;
-        end
-    end
-    if (i2cinit_state == I2CINIT_WRITE ||
-        i2cinit_state == I2CINIT_START) begin
-        cur_shift <= cur_shift + 1;
-        if (cur_shift == 3'd7) begin
-            cur_byte <= cur_byte + 1;
-            i2cinit_state <= I2CINIT_ACK;
-        end
-        sda_wr <= 1'b1 & (i2c_bytes[cur_byte] >> (7-cur_shift));
-    end
-    if (i2cinit_state == I2CINIT_ACK) begin
-        sda_wr <= 1'b1;
-        if (cur_byte == N_BYTES) begin
-            i2cinit_state <= I2CINIT_STOP;
-        end else begin
-            i2cinit_state <= I2CINIT_WRITE;
-        end
-    end
-    if (i2cinit_state == I2CINIT_START) begin
-        i2cinit_state <= I2CINIT_WRITE;
-    end
-    if (i2cinit_state == I2CINIT_STOP) begin
-        i2cinit_state <= I2CINIT_DONE;
-        sda_startstop <= 1'b0;
-    end
-end
 
-always @(negedge clk_sda) begin
-    if (i2cinit_state == I2CINIT_START) begin
-        scl_en <= 1'b1;
-        sda_startstop <= 1'b0;
+always @(posedge clk) begin
+    clk_cnt <= clk_cnt + 1;
+    if (clk_cnt == 0) begin
+        clk_scl <= ~clk_scl;
+        if (clk_scl == 1'b1) begin
+            if (i2cinit_state == I2CINIT_DONE) begin
+                scl_en <= 1'b0;
+            end
+        end
     end else begin
-        sda_startstop <= 1'b1;
-    end
-end
-
-always @(posedge clk_scl) begin
-    if (i2cinit_state == I2CINIT_DONE) begin
-        scl_en <= 1'b0;
+        clk_sda <= ~clk_sda;
+        if (clk_sda == 1'b1) begin
+            case (i2cinit_state)
+                I2CINIT_WAIT: begin
+                    wait_cycles = wait_cycles + 1;
+                    if (wait_cycles == 16'd20) begin
+                        i2cinit_state <= I2CINIT_START;
+                    end
+                end
+                I2CINIT_START, I2CINIT_WRITE: begin
+                    if (i2cinit_state == I2CINIT_START) begin
+                        i2cinit_state <= I2CINIT_WRITE;
+                    end
+                    cur_shift <= cur_shift + 1;
+                    if (cur_shift == 3'd7) begin
+                        cur_byte <= cur_byte + 1;
+                        i2cinit_state <= I2CINIT_ACK;
+                    end
+                    sda_wr <= 1'b1 & (i2c_bytes[cur_byte] >> (7-cur_shift));
+                end
+                I2CINIT_ACK: begin
+                    sda_wr <= 1'b1;
+                    if (cur_byte == N_BYTES) begin
+                        i2cinit_state <= I2CINIT_STOP;
+                    end else begin
+                        i2cinit_state <= I2CINIT_WRITE;
+                    end
+                end
+                I2CINIT_STOP: begin
+                    i2cinit_state <= I2CINIT_DONE;
+                    sda_startstop <= 1'b0;
+                end
+            endcase
+        end else begin
+            if (i2cinit_state == I2CINIT_START) begin
+                scl_en <= 1'b1;
+                sda_startstop <= 1'b0;
+            end else begin
+                sda_startstop <= 1'b1;
+            end
+        end
     end
 end
 
