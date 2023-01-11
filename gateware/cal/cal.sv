07d5d0953c91388189f198601466817affa269c8
Sebastian Holzapfel <me@vk2seb.com>
2023-01-11 15:57:43 +0100

    gateware/cal: parameterize bitwidth

--- gateware/cal/cal.sv
+++ gateware/cal/cal.sv
@@ -11,47 +11,54 @@
 // documented in `cal.py`. This module only uses a single multiplier for
 // all channels such that there are plenty left over for user logic.
 
-module cal (
+`default_nettype none
+
+module cal #(
+    parameter integer W = 16, // sample width
+    parameter CAL_MEM_FILE = "cal_mem.hex"
+)(
     input clk, // 24Mhz
     input sample_clk,
-    input signed [15:0] in0,
-    input signed [15:0] in1,
-    input signed [15:0] in2,
-    input signed [15:0] in3,
-    input signed [15:0] in4,
-    input signed [15:0] in5,
-    input signed [15:0] in6,
-    input signed [15:0] in7,
-    output logic signed [15:0] out0,
-    output logic signed [15:0] out1,
-    output logic signed [15:0] out2,
-    output logic signed [15:0] out3,
-    output logic signed [15:0] out4,
-    output logic signed [15:0] out5,
-    output logic signed [15:0] out6,
-    output logic signed [15:0] out7
+    input signed [W-1:0] in0,
+    input signed [W-1:0] in1,
+    input signed [W-1:0] in2,
+    input signed [W-1:0] in3,
+    input signed [W-1:0] in4,
+    input signed [W-1:0] in5,
+    input signed [W-1:0] in6,
+    input signed [W-1:0] in7,
+    output logic signed [W-1:0] out0,
+    output logic signed [W-1:0] out1,
+    output logic signed [W-1:0] out2,
+    output logic signed [W-1:0] out3,
+    output logic signed [W-1:0] out4,
+    output logic signed [W-1:0] out5,
+    output logic signed [W-1:0] out6,
+    output logic signed [W-1:0] out7
 );
 
-localparam CAL_ST_ZERO      = 3'd0,
-           CAL_ST_MULTIPLY  = 3'd1,
-           CAL_ST_CLAMPL    = 3'd2,
-           CAL_ST_OUT       = 3'd3,
-           CAL_ST_HALT      = 3'd4;
+localparam int N_CHANNELS = 8;
+
+localparam int CAL_ST_ZERO      = 3'd0,
+               CAL_ST_MULTIPLY  = 3'd1,
+               CAL_ST_CLAMPL    = 3'd2,
+               CAL_ST_OUT       = 3'd3,
+               CAL_ST_HALT      = 3'd4;
 
 // Only need to clamp negative values as with current hardware it
 // is impossible to overflow in the positive direction during cal.
-localparam CLAMPL = -32'sd32000;
+localparam int signed CLAMPL = -32'sd32000;
 
-logic signed [15:0] cal_mem [0:15];
-logic signed [15:0] in      [0:7];
-logic signed [31:0] out     [0:7];
-logic        [2:0]  ch = 3'd0;
-logic        [2:0]  state = CAL_ST_ZERO;
+logic signed [W-1:0]     cal_mem [2*N_CHANNELS];
+logic signed [W-1:0]     in      [N_CHANNELS];
+logic signed [(2*W)-1:0] out     [N_CHANNELS];
+logic        [2:0]       ch      = 0;
+logic        [2:0]       state   = CAL_ST_ZERO;
 logic               l_sample_clk = 1'd0;
 
 // Calibration memory for 8 channels stored as
 // 2 bytes shift, 2 bytes multiply * 8 channels.
-initial $readmemh("cal_mem.hex", cal_mem);
+initial $readmemh(CAL_MEM_FILE, cal_mem);
 
 always_ff @(posedge clk) begin
 
@@ -74,11 +81,11 @@ always_ff @(posedge clk) begin
     case (state)
         CAL_ST_ZERO: begin
             out[ch] <= (in[ch] - cal_mem[{ch, 1'b0}]);
-            if (ch == 7) state <= CAL_ST_MULTIPLY;
+            if (ch == N_CHANNELS-1) state <= CAL_ST_MULTIPLY;
         end
         CAL_ST_MULTIPLY: begin
             out[ch] <= (out[ch] * cal_mem[{ch, 1'b1}]) >>> 10;
-            if (ch == 7) state <= CAL_ST_OUT;
+            if (ch == N_CHANNELS-1) state <= CAL_ST_OUT;
         end
         CAL_ST_OUT: begin
             // TODO(sebholzapfel): add CLAMPL to pipeline.
@@ -91,6 +98,9 @@ always_ff @(posedge clk) begin
             out6  <= out[6] < CLAMPL ? CLAMPL : out[6];
             out7  <= out[7] < CLAMPL ? CLAMPL : out[7];
             state <= CAL_ST_HALT;
+        end
+        default: begin
+            // Halt and do nothing.
         end
     endcase
 
