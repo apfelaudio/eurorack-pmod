07b31a789f0ff51b40196291d4b40a2e285556d5
Seb Holzapfel <me@vk2seb.com>
2023-08-15 14:51:16 +0200

    boards/ecpix-5: add and test support for LambdaConcept ECPIX-5 (#32)

---
+++ gateware/boards/ecpix5/sysmgr.v
@@ -0,0 +1,76 @@
+`default_nettype none
+
+module sysmgr (
+    // Assumed 100Mhz for ECPIX-5.
+	input  wire clk_in,
+	input  wire rst_in,
+	output wire clk_12m,
+	output wire rst_out
+);
+
+// Signals
+wire clk_fb;
+
+wire pll_lock;
+wire pll_reset;
+
+wire rst_i;
+reg [7:0] rst_cnt;
+
+`ifndef VERILATOR_LINT_ONLY
+
+// You can re-generate this using `ecppll` tool. Be careful, the default settings
+// disable PLLRST_ENA and use a different FEEDBK_PATH, make sure they remain.
+
+(* FREQUENCY_PIN_CLKI="100" *)
+(* FREQUENCY_PIN_CLKOP="12" *)
+(* ICP_CURRENT="12" *) (* LPF_RESISTOR="8" *) (* MFG_ENABLE_FILTEROPAMP="1" *) (* MFG_GMCREF_SEL="2" *)
+EHXPLLL #(
+        .PLLRST_ENA("ENABLED"),
+        .INTFB_WAKE("DISABLED"),
+        .STDBY_ENABLE("DISABLED"),
+        .DPHASE_SOURCE("DISABLED"),
+        .OUTDIVIDER_MUXA("DIVA"),
+        .OUTDIVIDER_MUXB("DIVB"),
+        .OUTDIVIDER_MUXC("DIVC"),
+        .OUTDIVIDER_MUXD("DIVD"),
+        .CLKI_DIV(25),
+        .CLKOP_ENABLE("ENABLED"),
+        .CLKOP_DIV(50),
+        .CLKOP_CPHASE(24),
+        .CLKOP_FPHASE(0),
+        .FEEDBK_PATH("INT_OP"),
+        .CLKFB_DIV(3)
+) pll_i (
+        .RST(pll_reset),
+        .STDBY(1'b0),
+        .CLKI(clk_in),
+        .CLKOP(clk_12m),
+        .CLKFB(clk_fb),
+        .CLKINTFB(clk_fb),
+        .PHASESEL0(1'b0),
+        .PHASESEL1(1'b0),
+        .PHASEDIR(1'b1),
+        .PHASESTEP(1'b1),
+        .PHASELOADREG(1'b1),
+        .PLLWAKESYNC(1'b0),
+        .ENCLKOP(1'b0),
+        .LOCK(pll_lock)
+);
+
+`endif
+
+// PLL reset generation
+assign pll_reset = rst_in;
+// Logic reset generation
+always @(posedge clk_in)
+    if (!pll_lock)
+        rst_cnt <= 8'h0;
+    else if (~rst_cnt[7])
+        rst_cnt <= rst_cnt + 1;
+
+assign rst_i = ~rst_cnt[7];
+
+assign rst_out = rst_i;
+
+endmodule // sysmgr
