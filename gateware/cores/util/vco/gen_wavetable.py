052d6b01d423a1cdfd7d53b97abbd03aac06b09d
Seb Holzapfel <me@vk2seb.com>
2023-08-23 08:19:55 +0200

    gateware: rewrite clocking architecture, improve ak4619 driver (#33)
    * Change default sample rate from ~96khz to ~48kHz and BICK from 12MHz -> 6MHz which improves stability a bit with long cables from FPGA/PMOD. There is no audible difference and the noise performance is a bit better.
    * Clean up clocking architecture so we can select any sample rate from 8kHz to 48kHz, removing fixed 12MHz assumptions everywhere.
    * Rewrite AK4619 driver to collapse most of the logic into one posedge block on clk_256fs.
    * Some other minor fixes so that this repository plays better with LiteX (splitting out the wavetable osc as its own module, bubbling up .hex config paths).
    * Compress ECP5 bitstreams so they upload faster
    
    TESTED on all supported platforms

--- gateware/cores/util/vco/gen_wavetable.py
+++ gateware/cores/util/vco/gen_wavetable.py
@@ -2,18 +2,23 @@
 
 import math
 
+import matplotlib.pyplot as plt
+
 N = 256
+
+values = []
 
 with open("wavetable.hex", "w") as f:
     for i in range(N):
         # 3 sine waves and a sawtooth on top of each other.
-        value = (1.0*math.sin(2*math.pi*i/N) +
-                 0.5*math.sin(4*math.pi*i/N) +
-                 0.3*math.sin(6*math.pi*i/N) +
-                 (0.2*i/N - 0.5))/2)
+        value = i/N - 0.5
         # Scale up to fit inside 16 bit integer and use 2s comp for negative.
         value = int((1<<14) * value)
+        values.append(value)
         if value < 0:
             value = 0xffff + value
         line = "{:04x}\n".format(value)
         f.write(line)
+
+plt.plot(values)
+plt.show()
