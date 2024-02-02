74e28bcd44f3f32082184a6a050b7d9d0a41f567
Sebastian Holzapfel <me@sebholzapfel.com>
2024-02-02 18:07:43 +0100

    Revert "sim/util: fix int conversion in i2c utils"
    This reverts commit 3038ef7d71ef58783e03df348fc59587bb0bcf45.

--- gateware/sim/util/i2s.py
+++ gateware/sim/util/i2s.py
@@ -21,16 +21,13 @@ async def i2s_clock_in_u32(bick, sdin):
 
 def bits_not(n, width):
     """Bitwise NOT from positive integer of `width` bits."""
-    n = int(n)
     return (1 << width) - 1 - n
 
 def bits_from_signed(n, width):
-    n = int(n)
     """Bits (2s complement) of `width` from signed integer."""
     return n if n >= 0 else bits_not(-n, width) + 1
 
 def signed_from_bits(n, width):
-    n = int(n)
     """Signed integer from (2s complement) bits of `width`."""
     if (1 << (width-1) & n) > 0:
         return -int(bits_not(n, width) + 1)
